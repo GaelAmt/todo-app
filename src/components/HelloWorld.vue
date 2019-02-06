03fd5ed736bad8956d11622e1f10681493aae50b
Gaël AMIOT <gael.amiot@efrei.net>
2019-02-06 11:57:08 +0100

    deteteThis changed

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -9,11 +9,11 @@
       <v-form v-model="valid">
         <v-text-field v-model="name" :rules="nameRules" label="Name" required></v-text-field>
         <v-text-field v-model="description" :rules="descriptionRules" label="Description" required></v-text-field>
-        <v-btn @click="submit" :disabled="!valid" color="success">submit</v-btn>
+        <v-btn @click="addTodo" :disabled="!valid" color="success"><i class="material-icons left">add</i></v-btn>
       </v-form>
     </v-flex>
     <v-flex xs6 offset-sm3>
-      <v-card v-for="item in todoItem" :key="item">
+      <v-card v-for="(item, index) in todoItem" :key="index">
         <v-card-title primary-title>
           <div>
             <h2>{{item.name}}</h2>
@@ -21,7 +21,7 @@
           </div>
         </v-card-title>
         <v-card-actions>
-          <v-btn @click="deleteThis(item.id)" color="error">
+          <v-btn @click="deleteThis(index)" color="error">
             <i class="material-icons left">delete</i>
           </v-btn>
         </v-card-actions>
@@ -34,8 +34,7 @@
 export default {
   data: () => ({
     title: "Todo App",
-    nextId: 1,
-    todoItem: [{ id: 0, name: "test", description: "test desc" }],
+    todoItem: [],
     valid: false,
     name: "",
     description: "",
@@ -43,22 +42,13 @@ export default {
     descriptionRules: [v => !!v || "Description is required"]
   }),
   methods: {
-    submit: function() {
-      let name = this.$data.name;
-      let description = this.$data.description;
-      let id = this.$data.nextId;
-      this.$data.nextId++;
-      this.$data.todoItem.push({ id, name, description });
+    addTodo: function() {
+      let name = this.name;
+      let description = this.description;
+      this.todoItem.push({ name, description });
     },
-    deleteThis: function(button_id) {
-      let i = 0;
-      for (let j = 0; j < this.$data.todoItem.length; j++) {
-        if (this.$data.todoItem[j].id == button_id) {
-          break;
-        }
-        i++;
-      }
-      this.$data.todoItem.splice(i, 1);
+    deleteThis: function(index) {
+      this.todoItem.splice(index, 1);
     }
   }
 };
