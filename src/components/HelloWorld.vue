c86bdb22599d0f2c91b7afb8d5fd074a74b365d8
Gaël AMIOT <gael.amiot@efrei.net>
2019-02-06 11:36:33 +0100

    layout changed

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,24 +1,32 @@
 <template>
   <v-container>
-    <div>
-      <h1>{{title}}</h1>
-    </div>
-    <v-form v-model="valid">
-      <v-text-field v-model="name" :rules="nameRules" label="Name" required></v-text-field>
-      <v-text-field v-model="description" :rules="descriptionRules" label="Description" required></v-text-field>
-      <v-btn @click="submit" :disabled="!valid" color="success">submit</v-btn>
-    </v-form>
-    <v-card v-for="item in todoItem" :key="item">
-      <v-card-title primary-title>
-        <div>
-          <h2>{{item.name}}</h2>
-          <div>{{item.description}}</div>
-        </div>
-      </v-card-title>
-      <v-card-actions>
-        <v-btn @click="deleteThis(item.id)" color="error"><i class="material-icons left">delete</i></v-btn>
-      </v-card-actions>
-    </v-card>
+    <v-flex xs6 offset-sm3>
+      <div>
+        <h1>{{title}}</h1>
+      </div>
+    </v-flex>
+    <v-flex xs6 offset-sm3>
+      <v-form v-model="valid">
+        <v-text-field v-model="name" :rules="nameRules" label="Name" required></v-text-field>
+        <v-text-field v-model="description" :rules="descriptionRules" label="Description" required></v-text-field>
+        <v-btn @click="submit" :disabled="!valid" color="success">submit</v-btn>
+      </v-form>
+    </v-flex>
+    <v-flex xs6 offset-sm3>
+      <v-card v-for="item in todoItem" :key="item">
+        <v-card-title primary-title>
+          <div>
+            <h2>{{item.name}}</h2>
+            <div>{{item.description}}</div>
+          </div>
+        </v-card-title>
+        <v-card-actions>
+          <v-btn @click="deleteThis(item.id)" color="error">
+            <i class="material-icons left">delete</i>
+          </v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-flex>
   </v-container>
 </template>
 
@@ -40,15 +48,15 @@ export default {
       let description = this.$data.description;
       let id = this.$data.nextId;
       this.$data.nextId++;
-      this.$data.todoItem.push({id, name, description });
+      this.$data.todoItem.push({ id, name, description });
     },
     deleteThis: function(button_id) {
       let i = 0;
-      for(let j = 0; j < this.$data.todoItem.length; j++) {
+      for (let j = 0; j < this.$data.todoItem.length; j++) {
         if (this.$data.todoItem[j].id == button_id) {
           break;
-        } 
-        i++
+        }
+        i++;
       }
       this.$data.todoItem.splice(i, 1);
     }
